cd69e3aad5dd561804f4d9331fe2a7d4ea0c9a1b
Taru Wiik <taruwiik@gmail.com>
2025-01-20 12:50:10 +0200

    Added Cat-class to handle cat-specific properties and methods

---
+++ PetFoodNutrientCalculator/Cat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFoodNutrientCalculator
+{
+    internal class Cat : Pet, IPet
+    {
+        private const double DAILY_TAURINE_NEED_PER_METABOLIC_KG = 9.9;
+
+        public double DailyTaurineNeed
+        {
+            get
+            {
+                return DailyTaurineNeed;
+            }
+            set
+            {
+                DailyTaurineNeed = MetabolicWeight * DAILY_TAURINE_NEED_PER_METABOLIC_KG;
+            }
+        }
+        
+            public Cat(string name, double weight)
+            {
+                Name = name;
+                Weight = weight;
+                MetabolicWeight = CalculateMetabolicWeight(weight);
+
+            }
+
+            public double CalculateMetabolicWeight(double weight)
+            {
+                double result = Math.Pow(weight, COEFFICIENT_FOR_METABOLIC_WEIGHT);
+                result = Math.Round(result, 2);
+
+                return result;
+            }
+
+            public int CalculateDailyEnergyRequirement()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void CalculateNutrients()
+            {
+                throw new NotImplementedException();
+            }
+    }
+}
