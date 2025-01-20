b631248e48b5929dbf1077c54d368879eeaa7f24
Taru Wiik <taruwiik@gmail.com>
2025-01-20 13:43:55 +0200

    Added an UserInteraction class to handle user input and actions, oraganization and maintainability

--- PetFoodNutrientCalculator/Program.cs
+++ PetFoodNutrientCalculator/Program.cs
@@ -4,16 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Dog testDog = new Dog("Pluto", 10.0);
-            Dog testDog1 = new Dog("Tessu", 12.0);
-
-            Cat testCat = new Cat("Whiskers", 4.0);
-
-            PetRepository petRepository = new PetRepository();
-            petRepository.Pets.Add(testCat);
-            petRepository.Pets.Add(testDog1);
-
-            Console.WriteLine(testCat.DailyTaurineNeed);
+            UserInteraction userInteraction = new UserInteraction();
+            userInteraction.Start();
         }
     }
 }
---
+++ PetFoodNutrientCalculator/UserInteraction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFoodNutrientCalculator
+{
+    public class UserInteraction
+    {
+        bool isValidInput = false;
+        int userInput;
+
+        public void Start()
+        {
+            while (!isValidInput)
+            {
+                Console.WriteLine("Valitse 1 mikäli haluat lisätä lemmikin.");
+                Console.WriteLine("Valitse 2 mikäli haluat valita lemmikin listalta");
+                Console.WriteLine("Valitse 3 mikäli haluat sulkea sovelluksen");
+
+                string input = Console.ReadLine();
+
+                try
+                {
+                    userInput = int.Parse(input);
+
+                    if (userInput == 1 || userInput == 2 || userInput == 3)
+                    {
+                        isValidInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Syöte virheellinen, valitse kokonaisluku 1, 2 tai 3");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Syöte virheellinen, valitse kokonaisluku 1, 2 tai 3.");
+                }
+
+            }
+        }
+    }
+}
+
+
+
