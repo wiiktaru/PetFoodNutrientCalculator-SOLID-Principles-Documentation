f3b9f0c337a0e16702784e5b9852396c2adb93fa
Taru Wiik <taruwiik@gmail.com>
2025-01-20 10:03:18 +0200

    Added Pet class to provide common properties, support inheritance and improve code organization and maintainability

---
+++ PetFoodNutrientCalculator/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFoodNutrientCalculator
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+    }
+}
