4acc756b6c80d49e91b614f6fbf1efc61731e867
Taru Wiik <taruwiik@gmail.com>
2025-01-20 09:19:48 +0200

    Added test project to ensure software quality, early bug detection, and provide documentation for testing procedures

---
+++ PetFoodNutriendCalculatorTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PetFoodNutriendCalculatorTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
