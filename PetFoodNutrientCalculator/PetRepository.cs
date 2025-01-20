d010e37d7587ae3794f25572d486f831a401b29c
Taru Wiik <taruwiik@gmail.com>
2025-01-20 12:25:26 +0200

    Added PetRepository Class for managing the collection of pets

---
+++ PetFoodNutrientCalculator/PetRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFoodNutrientCalculator
+{
+    public class PetRepository
+    {
+        public List<Pet> Pets { get; private set; }
+
+        public PetRepository()
+        {
+            Pets = new List<Pet>();
+        }
+    }
+}
--- PetFoodNutrientCalculator/Program.cs
+++ PetFoodNutrientCalculator/Program.cs
@@ -4,8 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Dog testDog = new Dog("Pluto", 10.0); 
-            Console.WriteLine(testDog.MetabolicWeight);
+            Dog testDog = new Dog("Pluto", 10.0);
+            Dog testDog1 = new Dog("Tessu", 12.0);
+
+            PetRepository petRepository = new PetRepository();
+            petRepository.Pets.Add(testDog);
+            petRepository.Pets.Add(testDog1);
+
+            for (int i = 0; i <petRepository.Pets.Count; i++)
+            {
+                Console.WriteLine(petRepository.Pets[i].Name);
+            }
         }
     }
 }
