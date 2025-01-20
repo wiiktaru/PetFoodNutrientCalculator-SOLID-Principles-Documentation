c45fc58474622170b9881ed04220712dd80e076b
Taru Wiik <taruwiik@gmail.com>
2025-01-20 17:32:32 +0200

    Evaluated CatTests class unit tests and  some variables renamed for clarity

--- PetFoodNutriendCalculatorTests/CatTests.cs
+++ PetFoodNutriendCalculatorTests/CatTests.cs
@@ -14,31 +14,31 @@ namespace PetFoodNutrientCalculatorTests
             public void Constructor_ShouldInitializeProperties()
             {
             // Arrange
-            string name = "Whiskers";
+            string name = "Lucifer";
             double weight = 4.5;
 
             // Act
             var cat = new Cat(name, weight);
 
             // Assert
-            Assert.AreEqual(name, cat.Name, "Nimen tulisi vastata nimeä Whiskers");
-            Assert.AreEqual(weight, cat.Weight, "Painon tulisi olla 4.5");
+            Assert.AreEqual(name, cat.Name, "Nimen tulisi vastata nimeä Lucifer.");
+            Assert.AreEqual(weight, cat.Weight, "Painon tulisi vastata painoa 4.5.");
             Assert.AreEqual(cat.CalculateMetabolicWeight(weight), cat.MetabolicWeight, 
-                "Metabolisen painon tulisi vastata laskettua metabolista painoa");
+                "Metabolisen painon tulisi vastata laskettua metabolista painoa.");
             }
 
             [TestMethod]
              public void CalculateMetabolicWeight_ShouldReturnCorrectValue()
             {
             // Arrange
-            var cat = new Cat("Whiskers", 4.5);
-            double expectedMetabolicWeight = Math.Round(Math.Pow(4.5, Cat.COEFFICIENT_FOR_METABOLIC_WEIGHT), 2);
+            var cat = new Cat("Lucifer", 4.5);
+            double actualMetabolicWeight = Math.Round(Math.Pow(4.5, Cat.COEFFICIENT_FOR_METABOLIC_WEIGHT), 2);
 
             // Act
-            double metabolicWeight = cat.CalculateMetabolicWeight(4.5);
+            double catsMetabolicWeight = cat.CalculateMetabolicWeight(4.5);
 
             // Assert
-            Assert.AreEqual(expectedMetabolicWeight, metabolicWeight, "Metabolisen painon tulisi vastata" +
+            Assert.AreEqual(actualMetabolicWeight, catsMetabolicWeight, "Metabolisen painon tulisi vastata" +
                 "laskettua metabolista painoa");
             }
 
@@ -46,11 +46,11 @@ namespace PetFoodNutrientCalculatorTests
             public void DailyTaurineNeed_ShouldReturnCorrectValue()
             {
             // Arrange
-            var cat = new Cat("Whiskers", 4.5);
-            double expectedTaurineNeed = cat.MetabolicWeight * Cat.DAILY_TAURINE_NEED_PER_METABOLIC_KG;
+            var cat = new Cat("Lucifer", 4.5);
+            double actualTaurineNeed = cat.MetabolicWeight * Cat.DAILY_TAURINE_NEED_PER_METABOLIC_KG;
 
             // Assert
-            Assert.AreEqual(expectedTaurineNeed, cat.DailyTaurineNeed, "Tauriinin määrän tulisi vastata" +
+            Assert.AreEqual(actualTaurineNeed, cat.DailyTaurineNeed, "Tauriinin määrän tulisi vastata" +
                 "laskettua tauriinan määrää");
             }
     }
