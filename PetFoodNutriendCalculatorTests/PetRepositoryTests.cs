5a709371c6556a266b99b284ca2992b8dbafaa28
Taru Wiik <taruwiik@gmail.com>
2025-01-27 18:52:51 +0200

    Removed test that tested construcor as this is not needed or good practice

--- PetFoodNutriendCalculatorTests/PetRepositoryTests.cs
+++ PetFoodNutriendCalculatorTests/PetRepositoryTests.cs
@@ -7,21 +7,10 @@ using System.Threading.Tasks;
 
 namespace PetFoodNutrientCalculatorTests
 {
-    // TODO refactor unit tests
+    // TODO refactor unit tests§
     [TestClass]
     public class PetRepositoryTests
     {
-        [TestMethod]
-        public void Constructor_ShouldInitializePetsList()
-        {
-            // Arrange & Act
-            var repository = new PetRepository();
-
-            // Assert
-            Assert.IsNotNull(repository.Pets);
-            Assert.AreEqual(0, repository.Pets.Count);
-        }
-
         [TestMethod]
         public void AddShouldAddPetToList()
         {
